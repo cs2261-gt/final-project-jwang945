09dc796e23500e46770e9d1d0d8066dffda15093
Jet Wang <jetwang@Jets-MacBook-Pro-2.local>
2020-04-21 22:51:39 -0400

    made mainscreen look nicer

--- mainscreen.h
+++ mainscreen.h
@@ -5,11 +5,11 @@
 //
 //	mainscreen, 256x256@4, 
 //	+ palette 256 entries, not compressed
-//	+ 64 tiles (t|f|p reduced) not compressed
+//	+ 17 tiles (t|f|p reduced) not compressed
 //	+ regular map (in SBBs), not compressed, 32x32 
-//	Total size: 512 + 2048 + 2048 = 4608
+//	Total size: 512 + 544 + 2048 = 3104
 //
-//	Time-stamp: 2020-04-08, 17:17:20
+//	Time-stamp: 2020-04-21, 22:50:07
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,8 +18,8 @@
 #ifndef GRIT_MAINSCREEN_H
 #define GRIT_MAINSCREEN_H
 
-#define mainscreenTilesLen 2048
-extern const unsigned short mainscreenTiles[1024];
+#define mainscreenTilesLen 544
+extern const unsigned short mainscreenTiles[272];
 
 #define mainscreenMapLen 2048
 extern const unsigned short mainscreenMap[1024];
